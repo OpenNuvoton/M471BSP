cc5fcbbceafb1e7f28e6aff65ac6943cec8d569f
kchuang1 <kchuang1@nuvoton.com>
2024-11-20 15:01:19 +0800

    1. Upgrade CMSIS to 6.1.0 	2. update project include path for CMSIS 6.1.0 	3. Add DSP source
    Change-Id: I40fc106820367931a2215f297112764c66dbf8aa

--- Library/CMSIS/Driver/Include/Driver_Common.h
+++ Library/CMSIS/Driver/Include/Driver_Common.h
@@ -15,7 +15,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * $Date: 20/06/17 2:35p $Revision:    V2.0
+ * $Date:        2. Feb 2017
+ * $Revision:    V2.0
  *
  * Project:      Common Driver definitions
  */
